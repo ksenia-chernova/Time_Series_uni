dfc713aeea261b62d017507017bc78f14f942733
ksenia-chernova <ksenia_chernva@yandex.ru>
2026-02-25 10:55:40 +0500

    task 1, point 2

--- 01_lab/task_1.py
+++ 01_lab/task_1.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# dax - Германия, smi - Швейцария, cac - Франция, ftse - Великобритания
 data = pd.read_csv("data/EuStockMarkets.csv", index_col=0)
 
 # 1. Существуют ли в наборе данных взаимосвязанные столбцы?
@@ -14,5 +15,15 @@ def point_1():
     sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", center=0, fmt='.2f')
     plt.show()
 
+# 2. Вывести среднее значение изучаемой величины и дисперсию
+def point_2():
+    mean_value = data.mean()
+    variance_values = data.var()
+
+    print("Среднее значение:")
+    print(mean_value)
+    print("Дисперсия:")
+    print(variance_values)
+
 if __name__=="__main__":
-    point_1()
+    point_2()
