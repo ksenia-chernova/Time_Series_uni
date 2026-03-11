e625032641e11bf15d795a1d8c0542485a5c73d4
ksenia-chernova <ksenia-chernva@yandex.ru>
2026-03-11 18:52:06 +0500

    point 5

--- 01_lab/task_1.py
+++ 01_lab/task_1.py
@@ -15,30 +15,41 @@ def graph(data):
 
 # 1. Существуют ли в наборе данных взаимосвязанные столбцы?
 def point_1(data):
-    correlation_matrix = data.corr()
-    print("Матрица корреляции:")
-    print(correlation_matrix)
-    print()
+    correlation_matrix_current_prices = data.corr()
+    print("Матрица корреляции исходных цен:")
+    print(correlation_matrix_current_prices)
+
+    correlation_matrix_everyday_changes = data.diff().dropna().corr()
+    print("Матрица корреляции ежедневных изменений:")
+    print(correlation_matrix_everyday_changes)
 
 # 2. Вывести среднее значение изучаемой величины и дисперсию
 def point_2(data):
     mean_value = data.mean()
     variance_values = data.var()
 
-    print("Среднее значение:")
-    print(mean_value)
-    print("Дисперсия:")
-    print(variance_values)
-    print()
+    print("Среднее значение: \n", mean_value)
+    print("Дисперсия: \n", variance_values)
 
 # 5. Построить гистограмму абсолютных значений и гистограмму разностей. Сделать выводы.
 def point_5(data):
-    pass
+    
+    # абсолютные значения
+    plt.figure(figsize=(12, 5))
+    data.hist(bins=50, edgecolor='black')
+
+    # разности
+    returns = data.diff().dropna()
+    plt.figure(figsize=(12, 5))
+    returns.hist(bins=50, edgecolor='black')
+    plt.show()
 
 if __name__== "__main__":
     # dax - Германия, smi - Швейцария, cac - Франция, ftse - Великобритания
     data = pd.read_csv("data/EuStockMarkets.csv")
-    # point_1(data)
-    point_2(data)
-    point_5(data)
     graph(data)
+    del data['rownames']
+    # point_1(data)
+    # point_2(data)
+    point_5(data)
+    
