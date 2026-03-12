bd9728ed6c1089c000a85b74fa7f46ebf9ef481e
ksenia-chernova <ksenia-chernva@yandex.ru>
2026-03-12 21:48:07 +0500

    проверка на стационарный ряд

--- 01_lab/task_2.py
+++ 01_lab/task_2.py
@@ -2,21 +2,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-data = pd.read_csv("data/AirQualityUCI.csv", sep=';')
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.ar_model import AutoReg
 
-data['Datetime'] = pd.to_datetime(data['Date'] + ' ' + data['Time'], format='%d/%m/%Y %H.%M.%S')
 
-data = data.sort_values('Datetime')
+def stationary_row(data):
+    pt08 = data['PT08.S3(NOx)'].dropna()
 
-fig, ax = plt.subplots(1)
-fig.autofmt_xdate()
+    result = adfuller(pt08)
+    print('ADF Statistic (PT08.S3(NOx)): %f' % result[0])
+    print('p-value (PT08.S3(NOx)): %f' % result[1])
+    print('Critical Values (PT08.S3(NOx)):')
+    for key, value in result[4].items():
+        print('\t%s: %.3f' % (key, value))
 
-plt.plot(data['Datetime'], range(len(data)))
+def graph(data):
+    data = data.sort_values('Datetime')
 
-xfmt = mdates.DateFormatter('%d-%m-%y %H:%M')
-ax.xaxis.set_major_formatter(xfmt)
+    fig, ax = plt.subplots(1)
+    fig.autofmt_xdate()
 
-plt.xticks(rotation=90)
+    xfmt = mdates.DateFormatter('%d-%m-%y %H:%M')
+    ax.xaxis.set_major_formatter(xfmt)
 
-plt.tight_layout()
-plt.show()
+    plt.xticks(rotation=90)
+    plt.plot(data['Datetime'], data['PT08.S3(NOx)'])
+    plt.show()
+
+if __name__ == "__main__":
+    data = pd.read_csv("data/AirQualityUCI.csv", sep=';')
+    data['Datetime'] = pd.to_datetime(data['Date'] + ' ' + data['Time'], format='%d/%m/%Y %H.%M.%S')
+    stationary_row(data)
+    graph(data)
